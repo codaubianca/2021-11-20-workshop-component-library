9fc01b14e7e0032a8a62946934f9801ef9f4811a
codaubianca <codau_bianca@yahoo.com>
2021-11-20 15:55:57 +0100

    Slider v01

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import internal from "stream";
 import "./Slider.css";
 
-interface SliderProps {}
+interface SliderProps {
+  min: number;
+  max: number;
+  stepSize: number;
+  onChange?: () => void;
+}
 
-export const Slider = ({ ...props }: SliderProps) => {
-  // --- YOUR CODE HERE ---
+/*
 
-  return <>Slider</>;
+*/
+export const Slider = ({ min=0, max=100, stepSize=1, ...props }: SliderProps) => {
+  const [value, setValue] = useState("0");
+
+  return (
+      <div className="slider-container">
+        <div className="inputBlock">
+          <input type="number" id="myInputBlock" min={min} max={max} value={value} onChange={(event) => setValue(event.target.value)}></input>
+          <label>Unit</label>
+        </div>
+        <input type="range" id="mySlider" className="slider" min={min} max={max} value={value} step={stepSize} onChange={(event) => setValue(event.target.value)}></input>
+      </div>
+  );
 };
